af251f19a2ee1b2df59451f0b06b16a3348157f6
An-Toxa <97772@mail.ru>
2016-12-24 19:18:34 +0300

    add testing and area cell fixed

--- MATRIX-CORE(v. 1.0)/js/script.js
+++ MATRIX-CORE(v. 1.0)/js/script.js
@@ -9,8 +9,8 @@ document.getElementsByTagName('head')[0].appendChild(script);
 $(document).ready(function() {
 
 //----------MATRIX CREATE STARTING--------------//
-	var sizeX = 9; //small 9; default value 17;  large 33;
-	var sizeY = 9; //small 9; default value 16;  large 33;
+	var sizeX = 12; //small 9; default value 17;  large 33;
+	var sizeY = 12; //small 9; default value 16;  large 33;
 	var amountCells = sizeX*sizeY; //amount cells
 	var className = ".matrix-cell";
 	var cellValue_0, cellValue_1, cellEven, cellOdd;
@@ -18,10 +18,11 @@ $(document).ready(function() {
 	var rndValue;
 	
 	function matrixCreate(sX,sY){
+//#########(NEED ADD CODE) inspection for the argument value!#########//
 		var cellNumber = 1; //create LOCAL var unique key for create unique key for the cell
 			for (var y = 1; y-1<sY; y++){
 				for (var x = 1; x-1<sX; x++){
-					$("#matrix").append('<div class="matrix-cell" data-x-y='+x+'-'+y+' data-cell-value="0" data-cell-number='+cellNumber+' data-cell-content = "0">'+x+':'+y+'</div>');
+					$("#matrix").append('<div class="matrix-cell" data-x='+x+' data-y='+y+' data-cell-value="0" data-cell-number='+cellNumber+' data-cell-content = "0">'+x+':'+y+'</div>');
 					cellNumber++;  
 				}
 				$("#matrix").append('<br/>');
@@ -30,7 +31,7 @@ $(document).ready(function() {
 	matrixCreate(sizeX,sizeY); // starting matrix
 //-----------------------ENDING--------------------//
 
-	$('.matrix-cell:first').attr('data-cell-content','marker');
+	$('.matrix-cell:first').attr('data-cell-content','house'); // change content of element  with 0  on "house"
 
 	function dataSample(className,dataName,value){ //func sample data of attributes 
 			return $(""+className+"["+dataName+" = '"+arguments[2]+"']"); //sample attribute 
@@ -56,8 +57,8 @@ $(document).ready(function() {
 			dataSample(className,"data-flag","flag").css({'background':'blue'});
 			cellNumber = $(this).data('cell-number');
 			cellValue = $(this).data('cell-value');
-			x = parseFloat($(this).data('x-y')[0]);
-			y = parseFloat($(this).data('x-y')[2]);
+			x = parseFloat($(this).data('x'));
+			y = parseFloat($(this).data('y'));
 			cellContent = $(this).data('cell-content');
 			cellContent = $(this).data('cell-content');
 			cellValueRnd = $(this).data('rnd');
@@ -87,30 +88,27 @@ $(document).ready(function() {
 	
 	function actionBorders (x,y){
 		var arr = new Array(); 
-		arr[0] = dataSample(className,"data-x-y",(x)+"-"+(y)).data('cell-number');
-		arr[1] = dataSample(className,"data-x-y",(x-1)+"-"+(y-1)).data('cell-number');
-		arr[2] = dataSample(className,"data-x-y",(x)+"-"+(y-1)).data('cell-number');
-		arr[3] = dataSample(className,"data-x-y",(x+1)+"-"+(y-1)).data('cell-number');
-		arr[4] = dataSample(className,"data-x-y",(x-1)+"-"+(y)).data('cell-number');
-		arr[5] = dataSample(className,"data-x-y",(x+1)+"-"+(y)).data('cell-number');
-		arr[6] = dataSample(className,"data-x-y",(x-1)+"-"+(y+1)).data('cell-number');
-		arr[7] = dataSample(className,"data-x-y",(x)+"-"+(y+1)).data('cell-number');
-		arr[8] = dataSample(className,"data-x-y",(x+1)+"-"+(y+1)).data('cell-number');	
+		arr[0] = $(""+className+"[data-x="+(x)+"][data-y="+(y)+"]").data('cell-number');
+		arr[1] = $(""+className+"[data-x="+(x-1)+"][data-y="+(y-1)+"]").data('cell-number');
+		arr[2] = $(""+className+"[data-x="+(x)+"][data-y="+(y-1)+"]").data('cell-number');
+		arr[3] = $(""+className+"[data-x="+(x+1)+"][data-y="+(y-1)+"]").data('cell-number');
+		arr[4] = $(""+className+"[data-x="+(x-1)+"][data-y="+(y)+"]").data('cell-number');
+		arr[5] = $(""+className+"[data-x="+(x+1)+"][data-y="+(y)+"]").data('cell-number');
+		arr[6] = $(""+className+"[data-x="+(x-1)+"][data-y="+(y+1)+"]").data('cell-number');
+		arr[7] = $(""+className+"[data-x="+(x)+"][data-y="+(y+1)+"]").data('cell-number');
+		arr[8] = $(""+className+"[data-x="+(x+1)+"][data-y="+(y+1)+"]").data('cell-number');
 		
 		for (var i = 1; i<arr.length; i++){
 			dataSample(className,"data-cell-number",arr[i]).css({'background':'lime'})
-			if (dataSample(className,"data-cell-number",arr[i]).data('cell-content') == "marker"){
+			if (dataSample(className,"data-cell-number",arr[i]).data('cell-content') == "house"){
 				alert('yes!!!');
 			}
 		}
-		if (dataSample(className,"data-cell-number",arr[0]).data('cell-content') == "marker"){
+		if (dataSample(className,"data-cell-number",arr[0]).data('cell-content') == "house"){
 				alert('yes!!!');
 			}
 	}
-	var keySelf = 8224;
-	var product = "MilkShake";
-	alert (keySelf);
-	console.log(product);
+	
 	
 	
 	matrixAnalyzer();
